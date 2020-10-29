a286968d8d3cdee9d727274138652e12a2a702b4
Kevin <knphillips01@gmail.com>
2020-10-29 16:40:44 -0500

    now connected to mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const products = require('./data/products');
+import express from 'express';
+import dotenv from 'dotenv';
+import connectDB from './config/db.js';
+import products from './data/products.js';
 
-dotenv.config()
+dotenv.config();
+
+connectDB();
 
 const app = express();
 
@@ -19,6 +22,9 @@ app.get('/api/products/:id', (req, res) => {
   res.json(product);
 });
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} node on ${PORT}`));
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} node on ${PORT}`)
+);
