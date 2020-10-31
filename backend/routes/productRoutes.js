3b0568246371eabb46565e555a3964a7ed4801a1
Kevin <knphillips01@gmail.com>
2020-10-31 16:03:58 -0500

    cleaned up productroutes in backend

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,35 +1,11 @@
 import express from 'express';
-import aysncHandler from 'express-async-handler';
 const router = express.Router();
-import Product from '../models/productModel.js';
+import {
+  getProducts,
+  getProductById
+} from '../controllers/productController.js'
 
-// @desc      Fetch all products
-// @route     GET /api/products
-// @access    Public
-router.get(
-  '/',
-  aysncHandler(async (req, res) => {
-    const products = await Product.find({});
-
-    res.json(products);
-  })
-);
-
-// @desc      Fetch single product
-// @route     GET /api/products/:id
-// @access    Public
-router.get(
-  '/:id',
-  aysncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id);
-
-    if (product) {
-      res.json(product);
-    } else {
-      res.status(404);
-      throw new Error('Product not found');
-    }
-  })
-);
+router.route('/').get(getProducts);
+router.route('/:id').get(getProductById);
 
 export default router;
