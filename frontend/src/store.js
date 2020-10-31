c87eb10c3d2712af354e698537b92e04db5ac26e
Kevin <knphillips01@gmail.com>
2020-10-31 13:39:50 -0500

    added cart reducer and cart action

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -5,13 +5,21 @@ import {
   productListReducer,
   productDetailsReducer,
 } from './reducers/productReducers';
+import { cartReducer } from './reducers/cartReducers';
 
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
+  cart: cartReducer,
 });
 
-const initialState = {};
+const cartITemsFromStorage = localStorage.getItem('cartItems')
+  ? JSON.parse(localStorage.getItem('cartItems'))
+  : [];
+
+const initialState = {
+  cart: { cartItems: cartITemsFromStorage },
+};
 
 const store = createStore(
   reducer,
