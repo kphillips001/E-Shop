34335817eff263eb52245fa28be4c3ebba499c42
Kevin <knphillips01@gmail.com>
2020-11-24 16:54:58 -0600

    added few more products

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -21,7 +21,7 @@ const Header = () => {
       <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
         <Container>
           <LinkContainer to='/'>
-            <Navbar.Brand>E-Shop</Navbar.Brand>
+            <Navbar.Brand>Phillips Electronics</Navbar.Brand>
           </LinkContainer>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
